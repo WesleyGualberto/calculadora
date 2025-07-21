d6c622e1f0b2c18e462a90a859e3698d239b1254
Wesley Gualberto <37596981+WesleyGualberto@users.noreply.github.com>
2025-07-21 07:16:18 -0300

    Feito JavaScript da calculadora 👍

--- js/script.js
+++ js/script.js
@@ -1,25 +1,79 @@
 var botaoNumero = document.querySelectorAll('.numeros')
 let numeroAtual = ""
-var numerosOP = 0
+let numeros = []
+let operacaoSelecionada = ""
 
-var operacao = document.querySelectorAll('.teclasOperacoes')
+var botoesOperacao = document.querySelectorAll('.teclasOperacoes')
 var display = document.querySelector('#display')
 var resultado = document.querySelector('.teclaResultado')
 var limparCalc = document.querySelector('.teclaLimpar')
+
+const operacoesFunc = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "x": (a, b) => a * b,
+    "/": (a, b) => a / b
+};
 
 botaoNumero.forEach(function (botaoN) {
     botaoN.addEventListener("click", function () {
         numeroAtual += botaoN.textContent
         display.textContent = numeroAtual
-        numerosOP = Number(numeroAtual)
     })
 })
 
-operacao.forEach(function (botaoOP) {
+botoesOperacao.forEach(function (botaoOP) {
     botaoOP.addEventListener("click", function () {
-        operacao = botaoOP.textContent
-        display.textContent = operacao
+        if (numeroAtual !== "") {
+            numeros.push(Number(numeroAtual))
+        }
+
+        operacaoSelecionada = botaoOP.textContent
         numeroAtual = ""
-        console.log(numerosOP)
+        display.textContent = operacaoSelecionada
     })
 })
+
+resultado.addEventListener("click", function () {
+    if (numeroAtual !== "") {
+        numeros.push(Number(numeroAtual))
+    }
+
+    if (!operacoesFunc[operacaoSelecionada]) {
+        display.textContent = "[Erro] ao fazer o calculo!"
+        return
+    }
+
+    if (numeros.length === 0) {
+        display.textContent = "[Erro] Nenhum número para calcular!"
+        return
+    }
+
+    if (numeros.length === 1) {
+        display.textContent = numeros[0]
+        numeroAtual = ""
+        numeros = []
+        operacaoSelecionada = ""
+        return
+    }
+
+    let operacao = operacoesFunc[operacaoSelecionada];
+
+    let primeiroNumero = numeros[0]
+    let restoNumeros = numeros.slice(1)
+
+    let resultadoFinal = restoNumeros.reduce(function(acc, val) {
+        return operacao(acc, val)
+    }, primeiroNumero)
+
+    display.textContent = resultadoFinal
+})
+
+function limparTudo() {
+    numeroAtual = ""
+    numeros = []
+    operacaoSelecionada = ""
+    display.textContent = "0"
+}
+
+limparCalc.addEventListener("click", limparTudo)
